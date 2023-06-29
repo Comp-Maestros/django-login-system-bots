5c5e30b63fe8094b866cd699b247565c8c0f5f7e
Kiash254 <codewithkiash@gmail.com>
2023-06-29 15:23:21 +0300

    added homepage templates

--- config/settings.py
+++ config/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
 STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/static',
+    os.path.join(BASE_DIR, 'frontend/static'),
 ]
 
 #media files
