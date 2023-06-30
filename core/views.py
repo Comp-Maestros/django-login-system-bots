7cf42159498528071f70cab2494760461e4e0b39
Kiash254 <codewithkiash@gmail.com>
2023-06-30 12:04:49 +0300

    added  abstract user

--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+#add decorators to the views that require login where the login login is on a diffrent app named accounts use fucction based veiws
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from accounts.models import CustomUser
+
 
 # Create your views here.
-
-#create home page with function based view home.html
+#add login decorators where the logic is on accounts app
+@login_required(login_url='accounts:login')
 def home(request):
-    return render(request,'home.html')
+    
+    return render(request, 'home.html')
